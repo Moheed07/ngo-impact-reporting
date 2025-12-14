3df4178176b1cb1726986dcda258ed7d643c3d91
Moheed07 <moheedsyed21@gmail.com>
2025-12-14 17:43:51 +0530

    Fix Supabase SSL self-signed certificate issue

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  },
-  family: 4
+  ssl: true
 });
 
 module.exports = pool;
